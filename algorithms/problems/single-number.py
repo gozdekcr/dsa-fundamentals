e5f1ee6a393ecf255e881214fc6a5e18440e1575
gozde <kcr.gozde@gmail.com>
2026-05-05 22:22:01 +0300

    minor fix

--- algorithms/problems/single-number.py
+++ algorithms/problems/single-number.py
@@ -1,18 +1,19 @@
 # Problem: Single Number
 # Approach: HashMap - O(n²)
 # Date: 10-03-2026
-# nums = [2,2,1]
+
+nums = [2,2,1]
 
 
-# def singleNumber(nums):
-#     for i in nums:
-#         if nums.count(i) == 1:
-#             print(i)
+def singleNumber(nums):
+    for i in nums:
+        if nums.count(i) == 1:
+            print(i)
         
-# singleNumber(nums)
+singleNumber(nums)
 
 # Problem: Single Number
-# Approach: Linear Search - O(n)
+# Approach: HashMap - O(n)
 # Date: 05-05-2026
 
 list = [2,2,1]
