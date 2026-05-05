c43803933c98f405655e42cc11489f3b59dac58c
gozde <kcr.gozde@gmail.com>
2026-05-05 22:18:54 +0300

    add new solution

--- algorithms/problems/single-number.py
+++ algorithms/problems/single-number.py
@@ -1,13 +1,29 @@
 # Problem: Single Number
-# Approach: Linear Search - O(n²)
+# Approach: HashMap - O(n²)
 # Date: 10-03-2026
+# nums = [2,2,1]
 
-nums = [2,2,1]
 
-
-def singleNumber(nums):
-    for i in nums:
-        if nums.count(i) == 1:
-            print(i)
+# def singleNumber(nums):
+#     for i in nums:
+#         if nums.count(i) == 1:
+#             print(i)
         
-singleNumber(nums)
+# singleNumber(nums)
+
+# Problem: Single Number
+# Approach: Linear Search - O(n)
+# Date: 05-05-2026
+
+list = [2,2,1]
+
+def single_number(list):
+    set = {}
+    for i in list:
+        if i in set:
+            set[i]-=1
+        else:
+            set[i] = 1
+    return max(set, key=set.get)
+
+print(single_number(list))
